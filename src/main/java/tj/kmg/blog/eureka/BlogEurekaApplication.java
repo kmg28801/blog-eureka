fc9231529fe108dfbc54298e30cfd1ed70a45e46
kmg28801 <kmg08801@gmail.com>
2022-03-04 20:18:23 +0900

    eureka service initialize

---
+++ src/main/java/tj/kmg/blog/eureka/BlogEurekaApplication.java
@@ -0,0 +1,16 @@
+package tj.kmg.blog.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BlogEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogEurekaApplication.class, args);
+	}
+
+}
